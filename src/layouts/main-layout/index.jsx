fe5726dac6cc6f4ab1770709e2422c9e997392d8
dika841 <randikaa1999@gmail.com>
2023-05-24 21:44:41 +0700

    add fitur filter dan fix bug update todo

--- src/layouts/main-layout/index.jsx
+++ src/layouts/main-layout/index.jsx
@@ -2,7 +2,9 @@
 import PropTypes from "prop-types";
 const MainLayout = ({ children }) => {
   return (
-    <main className="w-full h-screen flex justify-center ">{children}</main>
+    <main className="w-full h-screen flex flex-col  items-center ">
+      {children}
+    </main>
   );
 };
 MainLayout.propTypes = { children: PropTypes.node.isRequired };
